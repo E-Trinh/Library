cd1ec9e3dcf056f1e42ae75a210048e4061b84f0
E-Trinh <ethantrinh@csus.edu>
2022-05-29 10:59:42 -0700

    Added add to library and display functions

--- book.js
+++ book.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -5,5 +7,33 @@ function Book(title, author, pages, read) {
     this.read = read;
     this.info = () => {
         return title + " by " + author + " , " + pages + " pages, " + read + " yet";
-    }
+    };
+}
+
+function addBookToLibrary() {
+    const title = prompt("title");
+    const author = prompt("author");
+    const pages = prompt("pages");
+    const read = prompt("read");
+    myLibrary.push(new Book(title, author, pages, read));
+}
+
+function displayBook() {
+    const body = document.querySelector("body");
+    myLibrary.forEach((currentValue) => {
+        const bookDiv = document.createElement("div");
+        const titleHeader = document.createElement("h1");
+        const authorHeader = document.createElement("p");
+        const pagesHeader = document.createElement("p")
+        const readHeader = document.createElement("p");
+        titleHeader.textContent = currentValue.title;
+        authorHeader.textContent = currentValue.author;
+        pagesHeader.textContent = currentValue.pages;
+        readHeader.textContent = currentValue.read;
+        bookDiv.appendChild(titleHeader);
+        bookDiv.appendChild(authorHeader);
+        bookDiv.appendChild(pagesHeader);
+        bookDiv.appendChild(readHeader);
+        body.appendChild(bookDiv);
+    });
 }
